020acee25cde72299158e7446ea46f24b2dcda44
Ali Ashraf <alyashrafelprince2004@gmail.com>
2025-07-31 20:40:07 +0300

    Debug closed and fixed delete logic bug

--- iti_project/settings.py
+++ iti_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mf!!avbtdclrk&+#(a-+6&smjim##wklbw%=m#1c()f43mu=v#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
